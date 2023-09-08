779fd37f3f2368e466339e1987af8c96e2ac3d55
hizki-rp <hizkielas@gmail.com>
2023-09-08 11:31:34 +0300

    question from db

--- index.php
+++ index.php
@@ -1,18 +1,36 @@
 <?php
 include "database.php";
-
- 
 $number = 1;
-// getting questions from table
-$query = "select * from `questions` where question_number = $number";
+$n = $number;
 
-$result = $mysqli->query($query) or die(mysqli->error.__LINE__);
+session_start();
 
-$question = $result->fetch_assoc();
-//getting choices from table
-$query = "select * from `choices` where question_number = $number";
+if(isset($_POST["1"]))
+{
+  display1();
+}
+else if(isset($_POST["2"]))
+{
+  display2();
+}
+else if(isset($_POST["3"]))
+{
+  display3();
+}
+else if(isset($_POST["4"]))
+{
+  display4();
+}
+else if(isset($_POST["5"]))
+{
+  display5();
+}
+else{
+  display1();
+}
 
-$choices = $mysqli->query($query) or die(mysqli->error.__LINE__);
+
+
 
 
 ?>
@@ -25,10 +43,10 @@ $choices = $mysqli->query($query) or die(mysqli->error.__LINE__);
     <link rel="stylesheet" href="./css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   </head>
-<body> 
+<body style="opacity: 0.955;"> 
 
   <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark lg-12 sm-4 justify-content-center align-items-center pt-4 pb-4">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark lg-12 sm-4 justify-content-center align-items-center pt-4 pb-4"">
      <a class="navbar-brand" href="#" >
        <h2>National Product Testing Clone</h2>
      </a>
@@ -42,49 +60,238 @@ $choices = $mysqli->query($query) or die(mysqli->error.__LINE__);
    
 <main>
 
-    <div class="row">
-      <div class="col-6 mx-auto">   
+    
+           <?php function display1()
+           {
+            global $number, $mysqli;
 
-        <div class="card align-items-center";>
-          <span style="font-size:large;" class="pt-4">
-            <?php echo $question['text']; ?>
-          </span>
-            <div class="card-body">
-                 <input type="hidden" name="ocs_p488348_1101680" value="No">
-                     <div class=" mb-4">
-                         <?php 
-                          while($row = $choices->fetch_assoc()) :?>
-                          <li class="pb-4" style="list-style:none;">
-                            <button type="button" class="btn btn-primary" style="height: 2.5rem; width:8rem;"><?php echo $row['text'] , "\n"; ?></button>
-                          </li>
-                         <?php endwhile;?>
-  
-                      </div>
-   
-               </div>
+            $query = "select * from `questions` where question_number = $number";
 
-        </div>
-      </div>
+            $result = $mysqli->query($query) or die(mysqli->error.__LINE__);
+            
+            $question = $result->fetch_assoc();
+            //getting choices from table
+            $query = "select * from `choices` where question_number = $number";
+            
+            $choices = $mysqli->query($query) or die(mysqli->error.__LINE__);
+            
+                        
+
+            ?>
+            <div class="row">
+          <div class="col-6 mx-auto">   
         
 
-  <div id="565995" class="ad-placement-item" data-creative_id="1101870" data-tracking_id="DCCC550C41D711EE82B862BDC67C7D5DD0ACF02B" data-click_url="https://clk.flexdirectpath.com/c/c=1101870/s=291705/p=692/m=DCCC550C41D711EE82B862BDC67C7D5DD0ACF02B/" style="display: none;">
-    <span>Whats the model of your car?</span>
-      <div class="survey-answers">
-              <input type="hidden" name="ocs_p488348_1101870">
-              <input type="text">
-              <button type="button">Done</button>
-              
-              
-      </div>
+        <div class="card align-items-center" style="border:none";>
+          <span style="font-size:large;" class="pt-4">
+          
+          <form action="" method="post">
+            
+            <strong> <?php echo $number; echo $question['text']; ?> </strong>
+          </span>
+            <div class="card-body">
 
-      <div id="634129" class="ad-placement-item" data-creative_id="1838521" data-tracking_id="DCCCB34441D711EE82B862BDC67C7D5DD0AC1663" data-click_url="https://clk.flexdirectpath.com/c/c=1838521/s=291705/p=692/m=DCCCB34441D711EE82B862BDC67C7D5DD0AC1663/" style="display: none;">
-       <span>Did you employ 5 or more W2 employees for 2020 and 2021? (1099s Do not count)</span>
-         <div class="survey-answers">
-              <input type="hidden" name="ocs_p488348_1838521">
-              <div>Yes - 5 or more employees</div>
-              <div>No - 4 or less employees</div>
+                     <div class=" mb-4">
+                     <input type="hidden" name="counter" value="<?php echo $number; ?>" />
+                    <?php while($row = $choices->fetch_assoc()): ?>   
+                      
+                          <input type="button" name="button" class="btn btn-primary button" value=<?php echo $row['text']; ?> style="margin-bottom:20px;" ><br>
+                          <?php endwhile; ?>   
+                         
+                          <input type="submit" name="2" value="2" />
+
+                         </form>
+                    <?php }
+                    ?>
+
+<?php function display2()
+           {
+            global $number, $mysqli;
+
+            $number += 1;
+
+            $query = "select * from `questions` where question_number = 2";
+
+            $result = $mysqli->query($query) or die(mysqli->error.__LINE__);
+            
+            $question = $result->fetch_assoc();
+            //getting choices from table
+            $query = "select * from `choices` where question_number = 2";
+            
+            $choices = $mysqli->query($query) or die(mysqli->error.__LINE__);
+            
+                        
+
+            ?>
+            <div class="row">
+          <div class="col-6 mx-auto">   
+        
+
+        <div class="card align-items-center" style="border:none";>
+          <span style="font-size:large;" class="pt-4">
+          
+          <form action="" method="post">
+            
+            <strong> <?php echo 2; echo $question['text']; ?> </strong>
+          </span>
+            <div class="card-body">
+
+                     <div class=" mb-4">
+                     <input type="hidden" name="counter" value="<?php echo 2; ?>" />
+                    <?php while($row = $choices->fetch_assoc()): ?>   
+                      
+                          <input type="button" name="button" class="btn btn-primary button" value=<?php echo $row['text']; ?> style="margin-bottom:20px;" ><br>
+                          <?php endwhile; ?>   
+                         
+                          <input type="submit" name="3" value="3" />
+
+                         </form>
+                    <?php }
+                    ?>
+
+
+
+<?php function display3()
+           {
+            global $number, $mysqli;
+             
+            
+
+            $query = "select * from `questions` where question_number = 3";
+
+            $result = $mysqli->query($query) or die(mysqli->error.__LINE__);
+            
+            $question = $result->fetch_assoc();
+            //getting choices from table
+            $query = "select * from `choices` where question_number = 3";
+            
+            $choices = $mysqli->query($query) or die(mysqli->error.__LINE__);
+            
+                        
+
+            ?>
+            <div class="row">
+          <div class="col-6 mx-auto">   
+        
+
+        <div class="card align-items-center" style="border:none";>
+          <span style="font-size:large;" class="pt-4">
+          
+          <form action="" method="post">
+            
+            <strong> <?php echo 3; echo $question['text']; ?> </strong>
+          </span>
+            <div class="card-body">
+
+                     <div class=" mb-4">
+                     <input type="hidden" name="counter" value="<?php echo 3; ?>" />
+                    <?php while($row = $choices->fetch_assoc()): ?>   
+                      
+                          <input type="button" name="button" class="btn btn-primary button" value=<?php echo $row['text']; ?> style="margin-bottom:20px;" ><br>
+                          <?php endwhile; ?>   
+                         
+                          <input type="submit" name="4" value="4" />
+
+                         </form>
+                    <?php }
+                    ?>
+
+                    
+<?php function display4()
+           {
+            global $number, $mysqli;
+             
+            
+
+            $query = "select * from `questions` where question_number = 4";
+
+            $result = $mysqli->query($query) or die(mysqli->error.__LINE__);
+            
+            $question = $result->fetch_assoc();
+            //getting choices from table
+            $query = "select * from `choices` where question_number = 4";
+            
+            $choices = $mysqli->query($query) or die(mysqli->error.__LINE__);
+            
+                        
+
+            ?>
+            <div class="row">
+          <div class="col-6 mx-auto">   
+        
+
+        <div class="card align-items-center" style="border:none";>
+          <span style="font-size:large;" class="pt-4">
+          
+          <form action="" method="post">
+            
+            <strong> <?php echo 3; echo $question['text']; ?> </strong>
+          </span>
+            <div class="card-body">
+
+                     <div class=" mb-4">
+                     <input type="hidden" name="counter" value="<?php echo 3; ?>" />
+                    <?php while($row = $choices->fetch_assoc()): ?>   
+                      
+                          <input type="button" name="button" class="btn btn-primary button" value=<?php echo $row['text']; ?> style="margin-bottom:20px;" ><br>
+                          <?php endwhile; ?>   
+                         
+                          <input type="submit" name="5" value="5" />
+
+                         </form>
+                    <?php }
+                    ?>
+
+<?php function display4()
+           {
+            global $number, $mysqli;
+             
+            
+
+            $query = "select * from `questions` where question_number = 4";
+
+            $result = $mysqli->query($query) or die(mysqli->error.__LINE__);
+            
+            $question = $result->fetch_assoc();
+            //getting choices from table
+            $query = "select * from `choices` where question_number = 4";
+            
+            $choices = $mysqli->query($query) or die(mysqli->error.__LINE__);
+            
+                        
+
+            ?>
+            <div class="row">
+          <div class="col-6 mx-auto">   
+        
+
+        <div class="card align-items-center" style="border:none";>
+          <span style="font-size:large;" class="pt-4">
+          
+          <form action="" method="post">
+            
+            <strong> <?php echo 3; echo $question['text']; ?> </strong>
+          </span>
+            <div class="card-body">
+
+                     <div class=" mb-4">
+                     <input type="hidden" name="counter" value="<?php echo 3; ?>" />
+                    <?php while($row = $choices->fetch_assoc()): ?>   
+                      
+                          <input type="button" name="button" class="btn btn-primary button" value=<?php echo $row['text']; ?> style="margin-bottom:20px;" ><br>
+                          <?php endwhile; ?>   
+                         
+                          <input type="submit" name="5" value="5" />
+
+                         </form>
+                    <?php }
+                    ?>
+                   
+                      </div>
    
-          </div>
+             
+              </li>
+
 
 
 </main>
@@ -95,10 +302,15 @@ $choices = $mysqli->query($query) or die(mysqli->error.__LINE__);
       
     </div>
 
-    
+   
+   
   </footer>
 
-   
+
+
+
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
+
+
